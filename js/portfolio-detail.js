5e88dce09a8dce65e71ba6d4492dc7490a86e71f
xenia530 <1003674769@qq.com>
2019-02-17 20:58:25 +0800

    change

--- js/portfolio-detail.js
+++ js/portfolio-detail.js
@@ -175,8 +175,8 @@ const portfolioList = [
 ]
 
 $(function(){
-	function getUrlParam(name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	function getUrlParam(name) { 
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象 
         var r = window.location.search.substr(1).match(reg);  //匹配目标参数
         console.log(r)
         if (r != null) return unescape(r[2]); return null; //返回参数值
